0652bc7e32df9355b7e1b3f7e0b9ccec81549037
mxnkarou <mxnkarou@icloud.com>
2023-03-29 03:14:45 +0200

    feat: base58 encoding

---
+++ src/crypto-extras/base58/check.rs
@@ -0,0 +1,107 @@
+use crate::crypto::encryption::sha256::DoubleSha256;
+use crate::crypto::encryption::FromSlice;
+use crate::crypto_extras::base58::encoding::b58_decode;
+use crate::crypto_extras::base58::encoding::b58_encode;
+use crate::wallet_sdk::network::BitcoinNetworkVersion;
+
+pub(crate) fn base58check_encode(hash: &[u8], network: BitcoinNetworkVersion) -> String {
+    let version = network as u8;
+    let mut payload = Vec::with_capacity(21);
+    payload.push(version);
+    payload.extend_from_slice(hash);
+
+    let sha = DoubleSha256::from_slice(&payload);
+    let bytes = sha.as_ref();
+
+    let checksum: [u8; 4] = bytes[0..4].try_into().unwrap();
+
+    let mut data = Vec::with_capacity(25);
+    data.push(version);
+    data.extend_from_slice(hash);
+    data.extend_from_slice(&checksum);
+
+    b58_encode(&data).unwrap()
+}
+
+pub(crate) fn base58check_decode(address: impl Into<String>) -> (Vec<u8>, BitcoinNetworkVersion) {
+    let address: String = address.into();
+
+    let buffer = b58_decode(address).unwrap().to_vec();
+
+    let checksum = &buffer[buffer.len() - 4..];
+    let data = buffer[1..buffer.len() - 4].to_vec();
+
+    let sha = DoubleSha256::from_slice(&buffer[0..buffer.len() - 4]);
+    let bytes = sha.as_ref();
+
+    for i in 0..4 {
+        assert_eq!(checksum[i], bytes[i]);
+    }
+
+    let prefix = buffer[0];
+
+    (data, BitcoinNetworkVersion::from(prefix))
+}
+
+mod tests {
+
+    #[test]
+    fn base58_test() {
+        use crate::crypto_extras::base58::check::base58check_decode;
+        use crate::crypto_extras::base58::check::base58check_encode;
+        use crate::wallet_sdk::network::BitcoinNetworkVersion;
+
+        let dummy = vec![
+            (
+                BitcoinNetworkVersion::MainnetP2PKH,
+                vec![
+                    "1FzTxL9Mxnm2fdmnQEArfhzJHevwbvcH6d",
+                    "1111111111111111111114oLvT2",
+                    "11111111111111111111BZbvjr",
+                    "12Tbp525fpnBRiSt4iPxXkxMyf5Ze1UeZu",
+                    "12Tbp525fpnBRiSt4iPxXkxMyf5ZWzA5TC",
+                ],
+            ),
+            (
+                BitcoinNetworkVersion::MainnetP2SH,
+                vec![
+                    "3GgUssdoWh5QkoUDXKqT6LMESBDf8aqp2y",
+                    "31h1vYVSYuKP6AhS86fbRdMw9XHieotbST",
+                    "31h1vYVSYuKP6AhS86fbRdMw9XHiiQ93Mb",
+                    "339cjcWXDj6ZWt9KBp4YxPKJ8BNH7gn2Nw",
+                    "339cjcWXDj6ZWt9KBp4YxPKJ8BNH14Nnx4",
+                ],
+            ),
+            (
+                BitcoinNetworkVersion::TestnetP2PKH,
+                vec![
+                    "mvWRFPELmpCHSkFQ7o9EVdCd9eXeUTa9T8",
+                    "mfWxJ45yp2SFn7UciZyNpvDKrzbhyfKrY8",
+                    "mfWxJ45yp2SFn7UciZyNpvDKrzbi36LaVX",
+                    "mgyZ7874UrDSCpvVnHNLMgAgqegGZBks3w",
+                    "mgyZ7874UrDSCpvVnHNLMgAgqegGQUXx9c",
+                ],
+            ),
+            (
+                BitcoinNetworkVersion::TestnetP2SH,
+                vec![
+                    "2N8EgwcZq89akxb6mCTTKiHLVeXRpxjuy98",
+                    "2MsFDzHRUAMpjHxKyoEHU3aMCMsVtMqs1PV",
+                    "2MsFDzHRUAMpjHxKyoEHU3aMCMsVtXMsfu8",
+                    "2MthpoMSYqBbuifmrrwgRaLJZLXaSyK2Rai",
+                    "2MthpoMSYqBbuifmrrwgRaLJZLXaSoxBM5T",
+                ],
+            ),
+        ];
+
+        for (version, addresses) in dummy {
+            for address in addresses {
+                let (decoded_data, decoded_version) = base58check_decode(address);
+                let encoded = base58check_encode(&decoded_data, decoded_version);
+
+                assert_eq!(encoded, address);
+                assert_eq!(decoded_version, version);
+            }
+        }
+    }
+}
