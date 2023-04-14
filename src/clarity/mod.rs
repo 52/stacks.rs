91ebc62952f25ffed43d42b0c443ec6bdf45658a
mxnkarou <mxnkarou@icloud.com>
2023-04-14 21:10:34 +0200

    chore: adjust clarity mod visibility

--- src/clarity/mod.rs
+++ src/clarity/mod.rs
@@ -1,42 +1,42 @@
 use std::fmt::Debug;
 use std::fmt::Display;
 
-use crate::clarity::bool::FalseCV;
-use crate::clarity::bool::TrueCV;
-use crate::clarity::buffer::BufferCV;
-use crate::clarity::int::IntCV;
-use crate::clarity::int::UIntCV;
-use crate::clarity::list::ListCV;
-use crate::clarity::optional::NoneCV;
-use crate::clarity::optional::SomeCV;
-use crate::clarity::principal::ContractPrincipalCV;
-use crate::clarity::principal::StandardPrincipalCV;
-use crate::clarity::response::ErrCV;
-use crate::clarity::response::OkCV;
-use crate::clarity::tuple::TupleCV;
+pub use crate::clarity::bool::FalseCV;
+pub use crate::clarity::bool::TrueCV;
+pub use crate::clarity::buffer::BufferCV;
+pub use crate::clarity::int::IntCV;
+pub use crate::clarity::int::UIntCV;
+pub use crate::clarity::list::ListCV;
+pub use crate::clarity::optional::NoneCV;
+pub use crate::clarity::optional::SomeCV;
+pub use crate::clarity::principal::ContractPrincipalCV;
+pub use crate::clarity::principal::StandardPrincipalCV;
+pub use crate::clarity::response::ErrCV;
+pub use crate::clarity::response::OkCV;
+pub use crate::clarity::tuple::TupleCV;
 
-pub mod bool;
-pub mod buffer;
-pub mod int;
-pub mod list;
-pub mod optional;
-pub mod principal;
-pub mod response;
-pub mod tuple;
+pub(crate) mod bool;
+pub(crate) mod buffer;
+pub(crate) mod int;
+pub(crate) mod list;
+pub(crate) mod optional;
+pub(crate) mod principal;
+pub(crate) mod response;
+pub(crate) mod tuple;
 
-pub const CLARITY_TYPE_INT: u8 = 0x00;
-pub const CLARITY_TYPE_UINT: u8 = 0x01;
-pub const CLARITY_TYPE_BUFFER: u8 = 0x02;
-pub const CLARITY_TYPE_BOOL_TRUE: u8 = 0x03;
-pub const CLARITY_TYPE_BOOL_FALSE: u8 = 0x04;
-pub const CLARITY_TYPE_PRINCIPAL_STANDARD: u8 = 0x05;
-pub const CLARITY_TYPE_PRINCIPAL_CONTRACT: u8 = 0x06;
-pub const CLARITY_TYPE_RESPONSE_OK: u8 = 0x07;
-pub const CLARITY_TYPE_RESPONSE_ERR: u8 = 0x08;
-pub const CLARITY_TYPE_OPTIONAL_NONE: u8 = 0x09;
-pub const CLARITY_TYPE_OPTIONAL_SOME: u8 = 0x0a;
-pub const CLARITY_TYPE_LIST: u8 = 0x0b;
-pub const CLARITY_TYPE_TUPLE: u8 = 0x0c;
+pub(crate) const CLARITY_TYPE_INT: u8 = 0x00;
+pub(crate) const CLARITY_TYPE_UINT: u8 = 0x01;
+pub(crate) const CLARITY_TYPE_BUFFER: u8 = 0x02;
+pub(crate) const CLARITY_TYPE_BOOL_TRUE: u8 = 0x03;
+pub(crate) const CLARITY_TYPE_BOOL_FALSE: u8 = 0x04;
+pub(crate) const CLARITY_TYPE_PRINCIPAL_STANDARD: u8 = 0x05;
+pub(crate) const CLARITY_TYPE_PRINCIPAL_CONTRACT: u8 = 0x06;
+pub(crate) const CLARITY_TYPE_RESPONSE_OK: u8 = 0x07;
+pub(crate) const CLARITY_TYPE_RESPONSE_ERR: u8 = 0x08;
+pub(crate) const CLARITY_TYPE_OPTIONAL_NONE: u8 = 0x09;
+pub(crate) const CLARITY_TYPE_OPTIONAL_SOME: u8 = 0x0a;
+pub(crate) const CLARITY_TYPE_LIST: u8 = 0x0b;
+pub(crate) const CLARITY_TYPE_TUPLE: u8 = 0x0c;
 
 #[derive(thiserror::Error, Clone, Debug, Eq, PartialEq)]
 pub enum Error {
